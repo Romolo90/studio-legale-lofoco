d865ca9e434a6c4791e2cd01b94dd99f0d174097
Romolo90 <81674420+Romolo90@users.noreply.github.com>
2024-12-30 01:25:26 +0100

    Update scipt.js

--- scipt.js
+++ scipt.js
@@ -1,1 +1,12 @@
-.
+// Riferimenti agli elementi
+const hamburgerMenu = document.getElementById
+const
+navLinks = document.getElementById( 'navl
+// Evento per mostrare/nascondere il menu
+hamburgerMenu.addEventListener( 'click', () =>
+navLinks.classList.toggle( 'nav-active');
+console. 1og ( 'Menu hamburger cliccato'); // [ console. 1og ( 'Classe nav-active presente:',
+});
+Expected branch to point to
+"035b8149d299a268f6fabab2f51853b20
+44f3225" but it did not. Pull and try again.
