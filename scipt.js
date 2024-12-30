aba493715d985e2b925b0a419ceb4861213f3d56
Romolo90 <81674420+Romolo90@users.noreply.github.com>
2024-12-30 01:33:07 +0100

    Update scipt.js

--- scipt.js
+++ scipt.js
@@ -1,10 +1,23 @@
 // Riferimenti agli elementi
+const scrollToTopBtn = document.getElementById('scrollToTop');
 const hamburgerMenu = document.getElementById('hamburgerMenu');
 const navLinks = document.getElementById('navLinks');
 
-// Evento per mostrare/nascondere il menu
+// Mostra/nasconde il pulsante "Torna su"
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    scrollToTopBtn.style.display = 'block';
+  } else {
+    scrollToTopBtn.style.display = 'none';
+  }
+});
+
+// Funzione per tornare in cima alla pagina
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+// Evento per il menu a tendina
 hamburgerMenu.addEventListener('click', () => {
   navLinks.classList.toggle('nav-active');
-  console.log('Menu hamburger cliccato');
-  console.log('Classe nav-active presente:', navLinks.classList.contains('nav-active'));
 });
